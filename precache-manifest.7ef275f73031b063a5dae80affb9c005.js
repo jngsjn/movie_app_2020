b967881e5cfbd2e60f552df00241ffe2bb0bfbc3
JNGSJN <urtxxt@jeongsejins-MacBook-Pro.local>
2020-06-02 23:49:52 +0900

    Updates

---
+++ precache-manifest.7ef275f73031b063a5dae80affb9c005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190e81693e27ae4e90d7c64738842495",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b0685e4408ff1c9eeb78",
+    "url": "/movie_app_2020/static/css/main.43b306d9.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0685e4408ff1c9eeb78",
+    "url": "/movie_app_2020/static/js/main.a322ae5b.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
